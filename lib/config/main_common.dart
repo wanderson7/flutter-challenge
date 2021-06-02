020a31875def5be9a17d4a869a17490ce2aa5297
Wanderson Santos <wandeerson7@gmail.com>
2021-06-02 20:58:09 -0300

    Add android permission internet

--- lib/config/main_common.dart
+++ lib/config/main_common.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_challenge/app/app_widget.dart';
 import 'package:flutter_challenge/app/shared/logger/logger_container.dart';
 import 'package:flutter_challenge/app/helpers/constants.dart';
 import 'package:flutter_challenge/app/shared/model/user/user_model.dart';
 import 'package:flutter_challenge/app/shared/user_preferences/shared_preferences_helper.dart';
 import 'package:provider/provider.dart';
 
-import '../app/app_widget.dart';
 
 final logger = LoggerContainer().init();
 
--- lib/config/production/main.dart
+++ lib/config/production/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter_challenge/config/environment.dart';
+import 'package:flutter_challenge/config/main_common.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import '../environment.dart';
-import '../main_common.dart';
+
 
 Future<void> main() async {
   await dotenv.load(fileName: 'lib/config/production/.env');
